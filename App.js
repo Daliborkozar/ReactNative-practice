5770211755d32f2a58c6ee479f70295c105b0845
Dalibor Kozar <daliborkozar@Dalibors-MacBook-Air.local>
2021-05-28 19:53:29 +0200

    fooling around with react native

--- App.js
+++ App.js
@@ -1,21 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native';
 
 export default function App() {
+  const [goal, setAddGoal] = useState('')
+  const [goalList, setGoalList] = useState([])
+  const [isSelected, setSelection] = useState(false);
+
+  const handleInput = (eText) => {
+    setAddGoal(eText)
+  }
+
+  const handleAdd = () => {
+
+    setGoalList([...goalList, goal])
+    setAddGoal('')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.goalAddcontainer}>
+        <TextInput
+          style={styles.textinp}
+          placeholder='add goal'
+          value={goal}
+          onChangeText={handleInput} />
+        <Button title="ADD" onPress={handleAdd} />
+      </View>
+      <View>
+        <ScrollView>
+        {goalList.map((item, i) => (
+          <View style={styles.goaltextWrap} key={i}>
+            <Text style={styles.goalText}>{item}</Text>
+          </View>
+
+        ))
+        }
+        </ScrollView>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 70
   },
+  textinp: {
+    borderWidth: 1,
+    borderColor: 'black',
+    padding: 5,
+    flex: 1,
+
+  },
+  goalAddcontainer: {
+    display: 'flex',
+    flexDirection: 'row',
+
+  },
+  goaltextWrap: {
+    padding: 10
+  },
+  goalText: {
+    fontSize: 20,
+    alignItems: 'center'
+  },
+
 });
